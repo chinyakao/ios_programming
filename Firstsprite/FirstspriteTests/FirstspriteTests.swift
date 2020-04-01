45e9bdf80dd688ae21d8561a2b2767f8e0394ae9
chinyakao <44396169+chinyakao@users.noreply.github.com>
2020-04-01 16:02:57 +0800

    sprite

---
+++ Firstsprite/FirstspriteTests/FirstspriteTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirstspriteTests.swift
+//  FirstspriteTests
+//
+//  Created by mac23 on 2020/4/1.
+//  Copyright © 2020 mac23. All rights reserved.
+//
+
+import XCTest
+@testable import Firstsprite
+
+class FirstspriteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
