354351d42b23252a56ddb6ce9cd24f5caaf13155
chinyakao <44396169+chinyakao@users.noreply.github.com>
2020-04-16 12:37:50 +0800

    STV

---
+++ STV/STV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STV
+//
+//  Created by mac24 on 2020/4/16.
+//  Copyright © 2020 mac24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
